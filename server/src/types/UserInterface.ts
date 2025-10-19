b9843ada799952d5d671240dc639fa63dd6a2cf6
ninitrinhnb <ninitrinh9@gmail.com>
2025-10-19 11:33:51 +0700

    Add files via upload

--- server/src/types/UserInterface.ts
+++ server/src/types/UserInterface.ts
@@ -8,14 +8,5 @@ export interface UserInterface {
   roles?: string
 }
 
-export interface EditUserInterface {
-  id: string
-  username?: string
-  email?: string
-  password?: string
-  removeRoleId?: string
-  addRoleId?: string
-}
-
 export type LoginType = Pick<UserInterface, 'email' | 'password'>
 export type RegisterType = Required<UserInterface>
