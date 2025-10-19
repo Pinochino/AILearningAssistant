b82cf638b50fa7ca761b19e07cc0447010367ad7
dt-u <dtu.mailm@gmail.com>
2025-10-19 19:09:48 +0700

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./redux/store"
 
-createRoot(document.getElementById('root')!).render(<App />)
+const container = document.getElementById("root")!
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+)
