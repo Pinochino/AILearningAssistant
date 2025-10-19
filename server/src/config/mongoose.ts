bcb4ebec48d547c4a52eddcc414af28dc21ffffc
dt-u <dtu.mailm@gmail.com>
2025-10-19 19:52:49 +0700

    Add files via upload

--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+export async function connectMongo(uri: string) {
+    console.log("🔍 Trying to connect MongoDB with URI:", uri); // debug
+
+    try {
+        await mongoose.connect(uri);
+        console.log("✅ MongoDB connected successfully!");
+    } catch (err) {
+        console.error("❌ MongoDB connection error:", err);
+        process.exit(1);
+    }
+}
