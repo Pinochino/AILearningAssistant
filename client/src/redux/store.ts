60b1c300652a0726e955112c559bd68e5008834c
dt-u <dtu.mailm@gmail.com>
2025-10-19 19:05:22 +0700

    Add files via upload

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,20 +2,21 @@ import { configureStore, Tuple } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { reducers } from './reducers/reducers'
-// Logger with default options
 import logger from 'redux-logger'
 
 const persistConfig = {
   key: 'root',
   storage,
-  backlist: ['auth']
+  blacklist: ['auth']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(new Tuple(logger))
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }).concat(new Tuple(logger))
 })
 
 export const persistor = persistStore(store)
